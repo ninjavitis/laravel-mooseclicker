91f64f7a54d8dc6677e171a1a0ed737b585573be
Peter Mackay <project2501@gmail.com>
2019-09-23 13:54:53 +0900

    updated procfile for heroku deployment

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Click the moose.</h1>
+  </div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token()}}">
+        <script>
+            window.Laravel = {csrfToken:'{{ csrf_token() }}'}
+        </script>
 
         <title>Mooseclicker</title>
 
@@ -12,6 +16,10 @@
         <!-- Styles -->
     </head>
     <body>
-        test
+        <div id="app">
+            <Home></Home>
+        </div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
