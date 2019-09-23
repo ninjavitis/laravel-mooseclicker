014f60477d00693ccc33ff985579afa6ba2c4f1e
Peter Mackay <project2501@gmail.com>
2019-09-23 16:51:04 +0900

    auth middleware, only return current user mooses

--- app/Http/Controllers/MooseController.php
+++ app/Http/Controllers/MooseController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MooseController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['index']);
+    }
+
+    public function index()
+    {
+        return response()->json(auth()->user()->mooses);
+    }
 }
